4947eebd1f0905e88f66f0ccf596f71e866c8823
Grzegorz Wasilko <wasilko1@o2.pl>
2021-10-04 19:33:26 +0200

    new templates 3

--- app.py
+++ app.py
@@ -2,29 +2,48 @@ from flask import Flask, render_template, url_for,request,redirect
 from flask_wtf import FlaskForm
 import json
 from forms import Library
-from models import books
+from models import Books
 
 app = Flask (__name__)
 app.config['SECRET_KEY'] = 'blabla'
 
+books=Books()                              #instancja  Books z pliku json
 
- 
 @app.route ('/', methods = ['GET','POST'])
 def index():
-    form = Library()
+    form = Library()                        #tworzy sie klasa formularza
     error = "Cos tu nie idzie jak trzeba "
-    if request.method == "POST":
+    if request.method == "POST":            #sprawdza czy walidacje (metody) sie powiodły 
         if form.validate_on_submit():       #spr czy dane do form zostały poprawnie wpisane .
-            books.create(form.data)
-            books.save_all()
-            xxx = books.get(2)
-            form = Library(data=books,xxx=xxx)#co tu sie dzieje
-            
-        return redirect(url_for('index',xxx=xxx))
-    return render_template('index.html', form=form, books=books, error=error)
+            books.create(form.data)         # dopisuje do objektu.klasyBooks dane z formularza
+            books.save_all()               # zapisuje stan pliku json 
+            return redirect(url_for('index')) #wracam do index
 
-    """if form.validate_on_submit():
-        return f'blabla'
-    return render_template("index.html", form=form)"""
+    return render_template('index.html', form=form, books=books.all(), error=error)# get powoduje wyswietlenie form i listy z books
+#________________________________________________________________________________________________________________
+@app.route ("/index/<int:books_id>", methods = ['GET','POST']) #czyli n 1 element jest pod http://127.0.0.1:5000/index/2
+def update(books_id):
+    position = books.get(books_id - 1) # do position wpisuje dane z listy o indeksie w id o 1 mniejszym
+    form = Library(data=position) # przekazuje do formularza Liblary dane z position 
+
+    if request.method == "POST":
+        if form.validate_on_submit():
+           books.update(id - 1, form.data)
+        return redirect(url_for('update'))
+    return render_template("index.html", form=form, id=books_id,books=books.all())
+#________________________________________________________________________________________________________
+@app.route ('/remove', methods = ['GET','POST'])
+def remove():
+    form = Library()                        #tworzy sie klasa formularza
+    error = "błędy  na stronie remove  "
+    if request.method == "POST":            #sprawdza czy walidacje (metody) sie powiodły 
+        if form.validate_on_submit():       #spr czy dane do form zostały poprawnie wpisane .
+            books.create(form.data)         # dopisuje do objektu.klasyBooks dane z formularza
+            books.remove()
+            books.save_all()               # zapisuje stan pliku json 
+            return redirect(url_for('index')) #wracam do index
+
+    return render_template('index.html', form=form, books=books.all(), error=error)# get powoduje wyswietlenie form i listy z books
+
   #  form = Library(data=books)
 
--- forms.py
+++ forms.py
@@ -1,12 +1,15 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, IntegerField, BooleanField,SelectField
+from wtforms.validators import DataRequired,required
 #from wtforms.validators import 
 
 
 class Library (FlaskForm):
     title = StringField (label='Book title')
-    id = IntegerField ('id')
+    id = IntegerField (label='id',validators=[DataRequired()])
     availble = BooleanField ('Availble')
     condition = SelectField ( 'good' , choices=[('good'),('bad condition'),('JakCieMoge')])
 
-Mylibrary =[{}]
+#Mylibrary =[{}]
+class id (FlaskForm):
+    ID=IntegerField (label='ID',validators=[DataRequired()])
